994fba7cae13b797b25ff2b989b9713fb266cdd3
Arfin Hayet <syed@primetechbd.com>
2024-11-30 13:08:12 +0600

    env removed

--- api/config/config.js
+++ api/config/config.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "avnadmin",
-    "password": process.env.db_password,
-    "database": "nextcommerze",
-    "host": "mysql-1032072a-arfinhayet786-cba6.j.aivencloud.com",
-    "port": 13826,
+    "username": "accesso3_bazar",
+    "password": "Tt123#123#Abc",
+    "database": "accesso3_bazar",
+    "host": "localhost",
+    "port": 3306,
     "dialect": "mysql"
   },
   "test": {
-    "username": "avnadmin",
-    "password": process.env.db_password,
-    "database": "nextcommerze",
-    "host": "mysql-1032072a-arfinhayet786-cba6.j.aivencloud.com",
-    "port": 13826,
+    "username": "accesso3_bazar",
+    "password": "Tt123#123#Abc",
+    "database": "accesso3_bazar",
+    "host": "localhost",
+    "port": 3306,
     "dialect": "mysql"
   },
   "production": {
