ab020d72380ab336430c02b7f0136535579b09bd
Arfin Hayet <syed@primetechbd.com>
2024-11-30 13:14:08 +0600

    env setup

--- api/config/config.js
+++ api/config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 module.exports = {
   "development": {
-    "username": "accesso3_bazar",
-    "password": "Tt123#123#Abc",
-    "database": "accesso3_bazar",
-    "host": "localhost",
-    "port": 3306,
+    "username": "avnadmin",
+    "password": process.env.db_password,
+    "database": "nextcommerze",
+    "host": "mysql-1032072a-arfinhayet786-cba6.j.aivencloud.com",
+    "port": 13826,
     "dialect": "mysql"
   },
   "test": {
